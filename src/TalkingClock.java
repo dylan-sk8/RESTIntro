d7ec45c3a20d5c00a131293116b6a3d4ebbb98c4
Dylan Montandon <dylan-sk8@hotmail.com>
2018-10-10 14:24:48 +0200

    talking clock done

--- src/TalkingClock.java
+++ src/TalkingClock.java
@@ -1,2 +1,16 @@
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Path("/time")
 public class TalkingClock {
+    @GET
+    @Produces("text/plain")
+    public String getTime() {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        return sdf.format(new Date());
+    }
+
 }
